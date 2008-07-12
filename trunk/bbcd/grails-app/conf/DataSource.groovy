3adcd069df9aba189ec107236c92a9323c9a2a28
jessbringlarsen <jessbringlarsen@d13b22f3-d219-0410-85e3-47ab0de77107>
2008-07-12 08:56:12 +0000

    Initial commit

---
+++ trunk/bbcd/grails-app/conf/DataSource.groovy
@@ -0,0 +1,32 @@
+dataSource {
+	pooled = true
+	driverClassName = "com.mysql.jdbc.Driver"
+	username = "bbcd"
+	password = "bbcd"
+}
+hibernate {
+    cache.use_second_level_cache=true
+    cache.use_query_cache=true
+    cache.provider_class='org.hibernate.cache.EhCacheProvider'
+}
+// environment specific settings
+environments {
+	development {
+		dataSource {
+			dbCreate = "update" // one of 'create', 'create-drop','update'
+			url = "jdbc:mysql://localhost/bbcd"
+		}
+	}
+	test {
+		dataSource {
+			dbCreate = "update"
+			url = "jdbc:mysql://localhost/bbcd"
+		}
+	}
+	production {
+		dataSource {
+			dbCreate = "update"
+			url = "jdbc:mysql://localhost/bbcd"
+		}
+	}
+}
