2ddda2a1cfa4807193128625227ee1ebe9966d02
jessbringlarsen <jessbringlarsen@d13b22f3-d219-0410-85e3-47ab0de77107>
2008-07-13 19:43:55 +0000

    Added testdata

--- bbcd/grails-app/conf/BootStrap.groovy
+++ bbcd/grails-app/conf/BootStrap.groovy
@@ -4,11 +4,52 @@ class BootStrap {
 
      def init = { servletContext ->
 
-     	new Idraetsforbund(name:"ØBTU").save()
-     	new Idraetsforbund(name:"JBTU").save()
-     	new Idraetsforbund(name:"FBTU").save()
-     	new Idraetsforbund(name:"BBTU").save()
-
+     	new Idraetsforbund(name:"ØBTU", xmlId:"1").save()
+     	new Idraetsforbund(name:"JBTU", xmlId:"2").save()
+     	new Idraetsforbund(name:"FBTU", xmlId:"3").save()
+     	new Idraetsforbund(name:"BBTU", xmlId:"4").save()
+     	
+     	Profile profile = new Profile(firstName:"Jess", 
+     			sirName:"Bring-Larsen",
+     			email:"jess@bringlarsen.dk",
+     			password:"hemmeligt",
+     			activationKey:"1",
+     			lastLogin:new Date(),
+     			activated:new Integer(1),
+     			question:"test",
+     			answer:"test")
+     	profile.save()
+     	
+     	new Team(name:"TeamOne",
+     			credit:new Integer(500),
+     			creationDate:new Date(),
+     			teamOwner:profile).save()
+     			
+     	new Team(name:"TeamTwo",
+     			credit:new Integer(1500),
+     			creationDate:new Date(),
+     			teamOwner:profile).save()
+     	
+     	Tournament tournament = new Tournament(name:"ØM",
+     			tournamentStart:new Date(),
+     			tournamentEnd: new Date(),
+     			isCurrentTournament:Boolean.TRUE)
+     	tournament.save()
+     			
+     	new League(name:"LeagueOne",
+     			creationDate:new Date(),
+		    	tournament:tournament,
+		    	privateLeague:Boolean.FALSE,
+		    	password:"hemmeligt",
+		    	leagueAdministrator:profile).save()
+		    	
+		new League(name:"LeagueTwo",
+     			creationDate:new Date(),
+		    	tournament:tournament,
+		    	privateLeague:Boolean.FALSE,
+		    	password:"hemmeligt",
+		    	leagueAdministrator:profile).save()
+     	
      	/*new PointsRule(from_:-10000, to_:-30, credit:-30000).save()
      	new PointsRule(from_:-29, to_:-20, credit:-20000).save()
      	new PointsRule(from_:-19, to_:-10, credit:-15000).save()
