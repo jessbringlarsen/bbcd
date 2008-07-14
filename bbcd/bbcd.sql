06ef8605140d4047f1db98ac489655275cf52bb6
jessbringlarsen <jessbringlarsen@d13b22f3-d219-0410-85e3-47ab0de77107>
2008-07-14 19:46:17 +0000

    Ported and added updateTeamRatingStat procedure

--- bbcd/bbcd.sql
+++ bbcd/bbcd.sql
@@ -51,3 +51,39 @@ BEGIN
 	where ss.rating_update = ratingupdateid;
 
 END|
+
+drop procedure if exists updateTeamRatingStat|
+create procedure updateTeamRatingStat()
+LANGUAGE SQL
+MODIFIES SQL DATA
+COMMENT 'For alle spillere på et sammensat hold, summér deres ratingtals frem- eller tilbage-gang'
+BEGIN
+	declare ratingupdate_id int(11);
+
+	-- Vælg den seneste ratingopdatering
+	select id
+	into ratingupdate_id
+	from rating_update_h;
+
+	-- Drop alle eventuelle eksisterende statistik rækker for det aktuelle ratingopdatering
+	delete from stat_team where rating_update_id = ratingupdate_id;
+
+	-- Indsæt rating statistik for hold
+	insert into stat_team (teamid, rating_update, rating_status)
+		(select tp.team_id, ratingupdate_id, sum(rating_status) from team_player tp
+		join player_stat ps on ps.player = tp.player_id
+		group by tp.team_id);
+
+	-- Indsæt point statistik for hold		
+	update team_stat ts set sh.credit_status = 
+		(SELECT sum(ps.credit_status)
+			FROM team_player tp
+				JOIN player_stat ps ON ps.player = tp.player_id
+			WHERE tp.team_id = ts.team_id
+				AND ps.raitng_update=ratingupdate_id);
+
+	-- Opdater placering for holdene
+	update team_stat sh set sh.position =
+   		(select @rownum:=@rownum+1 position from (select @rownum:=0) r)
+		where sh.rating_update = ratingupdate_id order by sh.rating_status desc;
+END|
