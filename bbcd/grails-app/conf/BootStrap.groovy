608de9729e86974663baeef3ff57fc515bad2421
jessbringlarsen <jessbringlarsen@d13b22f3-d219-0410-85e3-47ab0de77107>
2008-07-14 19:49:28 +0000

    Added PointsRuls test data

--- bbcd/grails-app/conf/BootStrap.groovy
+++ bbcd/grails-app/conf/BootStrap.groovy
@@ -39,26 +39,24 @@ class BootStrap {
      	new League(name:"LeagueOne",
      			creationDate:new Date(),
 		    	tournament:tournament,
-		    	privateLeague:Boolean.FALSE,
 		    	password:"hemmeligt",
 		    	leagueAdministrator:profile).save()
 		    	
 		new League(name:"LeagueTwo",
      			creationDate:new Date(),
 		    	tournament:tournament,
-		    	privateLeague:Boolean.FALSE,
 		    	password:"hemmeligt",
 		    	leagueAdministrator:profile).save()
      	
-     	/*new PointsRule(from_:-10000, to_:-30, credit:-30000).save()
-     	new PointsRule(from_:-29, to_:-20, credit:-20000).save()
-     	new PointsRule(from_:-19, to_:-10, credit:-15000).save()
-     	new PointsRule(from_:-9, to_:0, credit:-10000).save()
-     	new PointsRule(from_:1, to_:10, credit:10000).save()
-     	new PointsRule(from_:11, to_:15, credit:15000).save()
-     	new PointsRule(from_:21, to_:30, credit:20000).save()
-     	new PointsRule(from_:31, to_:10000, credit:30000).save()
-     	*/
+     	new PointsRule(fromRating:-10000, toRating:-30, credit:-30000).save()
+     	new PointsRule(fromRating:-29, toRating:-20, credit:-20000).save()
+     	new PointsRule(fromRating:-19, toRating:-10, credit:-15000).save()
+     	new PointsRule(fromRating:-9, toRating:0, credit:-10000).save()
+     	new PointsRule(fromRating:1, toRating:10, credit:10000).save()
+     	new PointsRule(fromRating:11, toRating:15, credit:15000).save()
+     	new PointsRule(fromRating:21, toRating:30, credit:20000).save()
+     	new PointsRule(fromRating:31, toRating:10000, credit:30000).save()
+     	
      }
 
      def destroy = {
