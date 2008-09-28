e32cadb8872d8eab233b6a5b3ec9861df04bbea2
jessbringlarsen <jessbringlarsen@d13b22f3-d219-0410-85e3-47ab0de77107>
2008-09-28 19:27:50 +0000

    ShortName attribute is not mandatory in external data supplier

--- trunk/bbcd/grails-app/domain/Club.groovy
+++ trunk/bbcd/grails-app/domain/Club.groovy
@@ -3,14 +3,12 @@ class Club {
     	columns {
     		xmlId column:'xml_id', index:'xml_id_idx'
     		name column:'name', index:'name_idx'
-    		shortName column:'short_name', index:'short_name_idx'
         }
     }
 	
 	static constraints = {
 		xmlId(unique: true)
 		name(unique: true)
-		shortName(unique: true)
 	}
 
 	String xmlId;
