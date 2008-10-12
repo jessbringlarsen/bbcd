93c92b8de360a48d8c83cea36cca3389db1b1110
jessbringlarsen <jessbringlarsen@d13b22f3-d219-0410-85e3-47ab0de77107>
2008-10-12 19:02:56 +0000

    clean up

--- bbcd/test/integration/RatingUpdateTest.groovy
+++ bbcd/test/integration/RatingUpdateTest.groovy
@@ -7,6 +7,9 @@ class RatingUpdateTest  extends GroovyTestCase {
 	 def RatingStatService ratingStatService
 	 def integrationTestHelper
 	 
+	 def firstUpdateRatings =  [1000, 500, 1500, 10, 0, 300, 1563, 4568]
+	 def secondUpdateRatings = [1000, 0, 2000, 11, 100, 1500, 1560, 4570]
+	 
 	 public void testRatingUpdateProcedures() {
 		integrationTestHelper = new IntegrationTestHelper()
 		 
@@ -21,11 +24,10 @@ class RatingUpdateTest  extends GroovyTestCase {
 		// Call stored procedure that update player_stat
 	    ratingStatService.updatePlayerStatitics()
 	    
-	    // Check player rating after update 
-	    def playerRatings = [0, -500, 500, 1, 100, 1200, -3, 2]
-	    for(i in 0..7) {
-	    	checkPlayer(integrationTestHelper.players[i], playerRatings[i])
-	    }
+	    // Check that the players have updated their rating properly
+	    checkPlayersStats()
+
+		// Call stored procedure that update team_stat
 	    ratingStatService.updateTeamStatitics()
 	    
 	    // Check team rating and their position
@@ -33,19 +35,32 @@ class RatingUpdateTest  extends GroovyTestCase {
 	    for(i in 0..3) {
 	    	checkTeam(integrationTestHelper.teams[i], teamRatings[i][0], teamRatings[i][1])
 	    }
+		
+		// Call stored procedure that update league_stat
 	    ratingStatService.updateLeagueStatitics()
+	    LeagueStat leagueStatOne = LeagueStat.findByLeague(integrationTestHelper.leagues[0])
+	    assert leagueStatOne.getPosition() == 2
+	    LeagueStat leagueStatTwo = LeagueStat.findByLeague(integrationTestHelper.leagues[1])
+	    assert leagueStatTwo.getPosition() == 1
 	}
 	 
 	 /**
 	  * Check that the playerStat table have been updated properly
 	  */
-	 private def checkPlayer(def player, def expectedRating) {
-		 def playerStats = PlayerStat.findAllByPlayer(player)
-		 assert playerStats.size() == 1
-		 assert playerStats[0].ratingStatus == expectedRating
+	 private def checkPlayersStats() {
 		 
-		 def pointsRule = PointsRule.findByFromRatingLessThanEqualsAndToRatingGreaterThanEquals(expectedRating, expectedRating)
-		 assert playerStats[0].creditStatus == pointsRule.credit
+		 def playerRatings = computePlayerRatingDiff()
+		 for(int i=0; i<firstUpdateRatings.size(); i++) {
+			def player = integrationTestHelper.players[i] 
+			def expectedRating = playerRatings[i]
+		 
+			def playerStats = PlayerStat.findAllByPlayer(player)
+			assert playerStats.size() == 1
+			assert playerStats[0].ratingStatus == expectedRating
+			 
+			def pointsRule = PointsRule.findByFromRatingLessThanEqualsAndToRatingGreaterThanEquals(expectedRating, expectedRating)
+			assert playerStats[0].creditStatus == pointsRule.credit
+		 }
 	 }
 	 
 	 /**
@@ -71,19 +86,45 @@ class RatingUpdateTest  extends GroovyTestCase {
 	 
 	 private def doFirstRatingUpdate() {
 		 def ratingUpdate = integrationTestHelper.ratingUpdates[0]
-		 def ratings = [1000, 500, 1500, 10, 0, 300, 1563, 4568]
 		 
-		 for(i in 0..7) {
-			 integrationTestHelper.doRatingUpdate(ratingUpdate, ratings[i], integrationTestHelper.players[i])
+		 for(int i=0; i<firstUpdateRatings.size(); i++) {
+			 integrationTestHelper.doRatingUpdate(ratingUpdate, firstUpdateRatings[i], integrationTestHelper.players[i])
 		 }
 	 }
 	 
 	 private def doSecondRatingUpdate() {
 		 def ratingUpdate = integrationTestHelper.ratingUpdates[1]
-		 def ratings = [1000, 0, 2000, 11, 100, 1500, 1560, 4570]
 		 
-		 for(i in 0..7) {
-			 integrationTestHelper.doRatingUpdate(ratingUpdate, ratings[i], integrationTestHelper.players[i])
+		 for(int i=0; i<firstUpdateRatings.size(); i++) {
+			 integrationTestHelper.doRatingUpdate(ratingUpdate, secondUpdateRatings[i], integrationTestHelper.players[i])
 		 }
+	 }
+	 
+	 /**
+	  * Compute the difference in player rating between the first and second
+	  * rating update
+	  */
+	 private def computePlayerRatingDiff() {
+		 def ratingDif = []
+		 
+		 for(int i=0; i<firstUpdateRatings.size(); i++) {
+			 ratingDif[i] = secondUpdateRatings[i] - firstUpdateRatings[i]
+		 }
+		 return ratingDif
+	 }
+	 
+	 /**
+	  * Compute the rating difference from the first to second 
+	  * rating update
+	  */
+	 private def computeTeamRatingDiff() {
+		 
+		 int j=1;
+		 for(int i=0; i<firstUpdateRatings.size()-1; i=i+2) {
+		     println "${firstUpdateRatings[i]} ${firstUpdateRatings[j]}"
+		     j=j+2
+		 }
+		 
+		 def teamRatings = [[-500, 4], [501, 2], [1300, 1], [-1, 3]]
 	 }
 }
