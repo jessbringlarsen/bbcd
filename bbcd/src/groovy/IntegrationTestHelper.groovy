298ceeaee96b1f761f74e92cf046e1b5adb81a0a
jessbringlarsen <jessbringlarsen@d13b22f3-d219-0410-85e3-47ab0de77107>
2008-10-28 21:27:31 +0000

    Adapted to new profile object

--- bbcd/src/groovy/IntegrationTestHelper.groovy
+++ bbcd/src/groovy/IntegrationTestHelper.groovy
@@ -1,10 +1,11 @@
 
 /**
  * Helper class for doing integration testing.
- * Responsible for setting up the needed domain objects
+ * Responsible for setting up the needed basic test data
  */
 class IntegrationTestHelper {
 	
+	def profile 
 	def idraetsforbund
 	def club
 	def players = []
@@ -21,6 +22,29 @@ class IntegrationTestHelper {
      	new Idraetsforbund(name:"BBTU", xmlId:"0004").save()
 		club = new Club(xmlId: '00001', name: 'BBC', shortName: 'BBC', union: idraetsforbund).save()
 
+		createPlayers()
+		
+		ratingUpdates[0] = new RatingUpdate(dateOfUpdate: new Date(100, 1, 1)).save()
+		ratingUpdates[1] = new RatingUpdate(dateOfUpdate: new Date(100, 1, 2)).save()
+		
+		
+        profile = new Profile(username:"jebl",
+                 userRealName:"Jess Bring-Larsen",
+                 email:"jess@bringlarsen.dk",
+                 passwd:"hemmeligt").save()
+
+		createTeams()
+     	createTeamPlayers()
+     	createLeagues()
+		createLeagueParticipants()    	
+		
+	}
+	
+	def doRatingUpdate(def ratingUpdate, def rating, def player) {
+        new Rating(ratingUpdate: ratingUpdate, rating: rating, player: player, club: club).save();
+	}
+	
+	private def createPlayers() {
 		for(i in 0..7) {
 			players[i] = new Player(xmlId: "0000${i}", 
                 playerNo: "${i}",
@@ -28,42 +52,33 @@ class IntegrationTestHelper {
                 gender: '1',
                 dateOfBirth: new Date()).save()
 		}
-				
-		ratingUpdates[0] = new RatingUpdate(dateOfUpdate: new Date(100, 1, 1)).save()
-		ratingUpdates[1] = new RatingUpdate(dateOfUpdate: new Date(100, 1, 2)).save()
-		
-		
-		Profile profile = new Profile(firstName:"Jess", 
-     			sirName:"Bring-Larsen",
-     			email:"jess@bringlarsen.dk",
-     			password:"hemmeligt",
-     			activationKey:"1",
-     			lastLogin:new Date(),
-     			activated:new Integer(1),
-     			question:"test",
-     			answer:"test")
-     	profile.save()
-
+	}
+	
+	private def createTeams() {
 		for(i in 0..3) {
 			teams[i] = new Team(name:"Team-${i}",
 	     			credit:new Integer(1500),
 	     			creationDate:new Date(),
 	     			teamOwner:profile).save()
 		}
-     			
-     	new TeamPlayer(team: teams[0], player:players[0]).save()
-     	new TeamPlayer(team: teams[0], player:players[1]).save()
+	}
+	
+	private def createTeamPlayers() {
+		new TeamPlayer(team:teams[0], player:players[0]).save()
+     	new TeamPlayer(team:teams[0], player:players[1]).save()
 		
-     	new TeamPlayer(team: teams[1], player:players[2]).save()
-     	new TeamPlayer(team: teams[1], player:players[3]).save()
+     	new TeamPlayer(team:teams[1], player:players[2]).save()
+     	new TeamPlayer(team:teams[1], player:players[3]).save()
      	
-     	new TeamPlayer(team: teams[2], player:players[4]).save()
-     	new TeamPlayer(team: teams[2], player:players[5]).save()
+     	new TeamPlayer(team:teams[2], player:players[4]).save()
+     	new TeamPlayer(team:teams[2], player:players[5]).save()
 		
-     	new TeamPlayer(team: teams[3], player:players[6]).save()
-     	new TeamPlayer(team: teams[3], player:players[7]).save()
-     			
-     	Tournament tournament = new Tournament(name:"ØM",
+     	new TeamPlayer(team:teams[3], player:players[6]).save()
+     	new TeamPlayer(team:teams[3], player:players[7]).save()
+	}
+	
+	private def createLeagues() { 
+		Tournament tournament = new Tournament(name:"ØM",
      			tournamentStart:new Date(),
      			tournamentEnd: new Date(),
      			isCurrentTournament:Boolean.TRUE)
@@ -80,14 +95,12 @@ class IntegrationTestHelper {
 		    	tournament:tournament,
 		    	password:"hemmeligt",
 		    	leagueAdministrator:profile).save()
-		    	
+	}
+	
+	private def createLeagueParticipants() {
 		new LeagueParticipant(league: leagues[0], team: teams[0]).save()
 		new LeagueParticipant(league: leagues[0], team: teams[1]).save()
 		new LeagueParticipant(league: leagues[1], team: teams[2]).save()
 		new LeagueParticipant(league: leagues[1], team: teams[3]).save()
-	}
-	
-	def doRatingUpdate(def ratingUpdate, def rating, def player) {
-        new Rating(ratingUpdate: ratingUpdate, rating: rating, player: player, club: club).save();
 	}
 }
