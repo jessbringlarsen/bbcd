1c51bbbeac13b77f85f7fcda13d6c7584787a070
jessbringlarsen@gmail.com <jessbringlarsen@gmail.com@d13b22f3-d219-0410-85e3-47ab0de77107>
2008-12-26 22:58:19 +0000

    Added service method to get all players not in a team

--- trunk/bbcd/grails-app/services/PlayerViewService.groovy
+++ trunk/bbcd/grails-app/services/PlayerViewService.groovy
@@ -13,5 +13,15 @@ class PlayerViewService {
             inner join p.teams as team
             where p.id = pw.id and team.id = ?""", [teamId])
     }
-	
+
+    def findAllByClubIdAndClassIdNotInTeam(clubId, classId, teamId) {
+        return PlayerView.executeQuery("""select pw from
+            PlayerView as pw
+            where pw.clubId = ?
+            and pw.classId = ?
+            and pw.id not in
+            (select player.id from Team as t, Player as p
+            inner join t.players as player where t.id = ?)
+            """, [clubId, classId, teamId])
+    }
 }
