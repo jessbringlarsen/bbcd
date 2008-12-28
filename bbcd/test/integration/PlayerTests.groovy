9658a08264bd52cca87f0bbe64dcbf61283d5989
jessbringlarsen@gmail.com <jessbringlarsen@gmail.com@d13b22f3-d219-0410-85e3-47ab0de77107>
2008-12-28 21:46:53 +0000

    Cleanup tests

--- bbcd/test/integration/PlayerTests.groovy
+++ bbcd/test/integration/PlayerTests.groovy
@@ -1,14 +1,15 @@
 class PlayerTests extends BaseBBCGroovyTestCase {
 
+
     void testNoOfPlayers() {
         def players = Player.list()
-        assert players.size() == 8
+        assert players.size() == testHelper.getTotalNumberOfPlayers()
     }
 
      void testNoOfPlayerTeams() {
         def players = Player.list()
         players.each {
-            assert it.getTeams().size() == 2
+            assert it.getTeams().size() == 1
         }
     }
 }
