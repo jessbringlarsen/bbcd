9c3fc671857da5bd6af6265969d512047a9cbc15
jessbringlarsen@gmail.com <jessbringlarsen@gmail.com@d13b22f3-d219-0410-85e3-47ab0de77107>
2009-01-31 18:42:35 +0000

    initial

---
+++ trunk/bbcd/plugins.sh
@@ -0,0 +1,10 @@
+grails install-plugin acegi 0.3
+grails install-plugin searchable 0.5
+
+# Remember to patch this plugin. Look in plugins-patch
+grails install-plugin ofchart 0.5
+grails install-plugin settings 1.0
+grails install-plugin yui 2.6.0
+grails install-plugin modalbox 0.3
+grails install-plugin bubbling 1.5.0
+grails install-plugin grails-ui 1.0
